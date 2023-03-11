9fa73b690bc72f16bc883e3deb3e674e3a452442
Hamzawy63 <hamzahassan835@gmail.com>
2023-03-11 23:27:10 +0200

    Added solution for the 4th riddle

--- gym-maze/riddle_solvers.py
+++ gym-maze/riddle_solvers.py
@@ -1,9 +1,24 @@
+from amazoncaptcha import AmazonCaptcha
+from PIL import Image
+import io
+import numpy as np
 
 def cipher_solver(question):
     return "HaCkTrIcK"
 
 def captcha_solver(question):
-    return ""
+    img = Image.fromarray(np.uint8(question))
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img = img_byte_arr.getvalue()
+    img = io.BytesIO(img)
+    
+    # Create an AmazonCaptcha object with the image
+    captcha = AmazonCaptcha(img)
+
+    # Get the captcha solution
+    solution = captcha.solve()
+    return solution
     
 
 def pcap_solver(question):
